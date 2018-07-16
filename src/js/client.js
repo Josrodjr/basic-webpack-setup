629287ef63fd13d13413941deef7cb01d1098a9f
Unknown <Josrodjr@gmail.com>
2018-07-15 19:50:54 -0600

    top left works
    turn changes

--- src/js/client.js
+++ src/js/client.js
@@ -1,11 +1,29 @@
 const state = {
-    pointsc1: ['', 'o', 'x'],
-    pointsc2: ['x', 'o', 'x'],
-    pointsc3: ['x', 'o', 'x'],
+    pointsc1: ['', '', ''],
+    pointsc2: ['x', 'x', 'x'],
+    pointsc3: ['o', 'o', 'o'],
     currentTurn: 0,
 };
 
 const render = lState => {
+
+    //tres divs en que dividimos el desde el root
+
+    const title = document.createElement('div');
+    title.className = 'title';
+    title.innerText = 'Totito';
+
+    const holder = document.createElement('div');
+    holder.className = 'holder';
+    
+    const turn = document.createElement('div');
+    turn.className = 'turn';
+    turn.innerText = 'Turno de: ';
+
+    //icono de turno
+    const icon = document.createElement('div');
+
+
     const col1 = document.createElement('div');
     col1.className = 'columna';
 
@@ -15,53 +33,96 @@ const render = lState => {
     const col3 = document.createElement('div');
     col3.className = 'columna';
 
+    //limpiar el rendering anterior
+    if (root.hasChildNodes()) {
+        root.innerHTML = null;
+      }
+
 
     //main rendering
-    root.appendChild(col1);
-    root.appendChild(col2);
-    root.appendChild(col3);
+    root.appendChild(title);
+    root.appendChild(holder);
+    root.appendChild(turn);
 
-    const botonesCol1 = lState.pointsc1.map(
-        (column, i) => {
-            const punto = document.createElement('button');
-            punto.className = `punto ${column}`;
-
-        return punto;
-        }
-    );
-
-    const botonesCol2 = lState.pointsc2.map(
-        (column, i) => {
-            const punto = document.createElement('button');
-            punto.className = `punto ${column}`;
-
-        return punto;
-        }
-    );
+    holder.appendChild(col1);
+    holder.appendChild(col2);
+    holder.appendChild(col3);
     
-    const botonesCol3 = lState.pointsc3.map(
-        (column, i) => {
-            const punto = document.createElement('button');
-            punto.className = `punto ${column}`;
 
-        return punto;
-        }
-    );
+    //agregar los botones manualmente
+    //boton0
+    const punto0 = document.createElement('button');
+    punto0.className = `punto ${state.pointsc1[0]}`;
 
-    // adjuntar los puntos por columna
-    botonesCol1.forEach(
-        punto => col1.appendChild(punto)
-    );
+    col1.appendChild(punto0);
 
-    // adjuntar los puntos por columna
-    botonesCol2.forEach(
-        punto => col2.appendChild(punto)
-    );
+    //boton1
+    const punto1 = document.createElement('button');
+    punto1.className = `punto ${state.pointsc1[1]}`;
 
-    // adjuntar los puntos por columna
-    botonesCol3.forEach(
-        punto => col3.appendChild(punto)
-    );
+    col1.appendChild(punto1);
+
+    //boton2
+    const punto2 = document.createElement('button');
+    punto2.className = `punto ${state.pointsc1[2]}`;
+
+    col1.appendChild(punto2);
+
+    //segunda columna
+    //boton3
+    const punto3 = document.createElement('button');
+    punto3.className = `punto ${state.pointsc2[0]}`;
+
+    col2.appendChild(punto3);
+
+    //boton4
+    const punto4 = document.createElement('button');
+    punto4.className = `punto ${state.pointsc2[1]}`;
+
+    col2.appendChild(punto4);
+
+    //boton5
+    const punto5 = document.createElement('button');
+    punto5.className = `punto ${state.pointsc2[2]}`;
+
+    col2.appendChild(punto5);
+
+    //tercera columna
+    //boton6
+    const punto6 = document.createElement('button');
+    punto6.className = `punto ${state.pointsc3[0]}`;
+
+    col3.appendChild(punto6);
+
+    //boton7
+    const punto7 = document.createElement('button');
+    punto7.className = `punto ${state.pointsc3[1]}`;
+
+    col3.appendChild(punto7);
+
+    //boton8
+    const punto8 = document.createElement('button');
+    punto8.className = `punto ${state.pointsc3[2]}`;
+
+    col3.appendChild(punto8);
+
+    // Events
+
+    //cambiar de color el div del turno
+    if(lState.currentTurn === 0){
+        icon.className = 'iconturn o'
+        turn.appendChild(icon);
+    } else {
+        icon.className = 'iconturn x'
+        turn.appendChild(icon);
+    }
+
+
+    punto0.onclick = () => {
+    lState.currentTurn = (lState.currentTurn + 1) % 2;
+    render(lState);
+    console.log(state.currentTurn);
+    };
 
 
 }
---
+++ src/js/test2.js
@@ -0,0 +1,75 @@
+const state = {
+    pointsc1: ['', 'o', 'x'],
+    pointsc2: ['x', 'o', 'x'],
+    pointsc3: ['x', 'o', 'x'],
+    currentTurn: 0,
+};
+
+const render = lState => {
+    const col1 = document.createElement('div');
+    col1.className = 'columna';
+
+    const col2 = document.createElement('div');
+    col2.className = 'columna';
+
+    const col3 = document.createElement('div');
+    col3.className = 'columna';
+
+
+    //main rendering
+    root.appendChild(col1);
+    root.appendChild(col2);
+    root.appendChild(col3);
+
+    const botonesCol1 = lState.pointsc1.map(
+        (column, i) => {
+            const punto = document.createElement('button');
+            punto.className = `punto ${column} ${i}`;
+
+        return punto;
+        }
+    );
+
+    const botonesCol2 = lState.pointsc2.map(
+        (column, i) => {
+            const punto = document.createElement('button');
+            punto.className = `punto ${column} ${i+3}`;
+
+        return punto;
+        }
+    );
+    
+    const botonesCol3 = lState.pointsc3.map(
+        (column, i) => {
+            const punto = document.createElement('button');
+            punto.className = `punto ${column} ${i+6}`;
+
+        return punto;
+        }
+    );
+
+    // adjuntar los puntos por columna
+    botonesCol1.forEach(
+        punto => col1.appendChild(punto)
+    );
+
+    // adjuntar los puntos por columna
+    botonesCol2.forEach(
+        punto => col2.appendChild(punto)
+    );
+
+    // adjuntar los puntos por columna
+    botonesCol3.forEach(
+        punto => col3.appendChild(punto)
+    );
+
+     // Events
+     punto1.onclick = () => {
+        lState.currentLight = (lState.currentLight + 1) % lState.lights.length;
+        render(lState);
+      };
+
+
+}
+
+render (state);
